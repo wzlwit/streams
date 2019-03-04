6f3c74952ae4fa92482b1b0e51da836b5471beac
Zhaolong Wang <zl.wangadfa@hotmail.com>
2019-03-04 15:07:18 -0500

    stream action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types';
 
 export const signIn = userid => {
     return {
@@ -15,5 +23,28 @@ export const signOut = () => {
 };
 
 export const createStream = formvalues => async dispatch => {
-    streams.post('/streams', formvalues);
+    const response = await streams.post('/streams', formvalues);
+
+    dispatch({ type: CREATE_STREAM, payload: response.data });
 }
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    console.log(response.data);
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+}
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+}
+
+export const editStream = (id, formvalues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formvalues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+}
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });// put (id) here for reducer
+}
+
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import {reducer as formReducer} from 'redux-form';
+import {reducer as formReducer} from 'redux-form'; //formReducer
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     auth: authReducer,
-    form:formReducer
+    form:formReducer,
+    streams:streamReducer
 });
